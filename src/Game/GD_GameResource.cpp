06e957ed8acdc4069727ce1e4502a5e458d33113
SADIO-M <megan_florence_sadio@dlsu.edu.ph>
2025-04-04 19:34:59 +0800

    New Atlas for UI

--- src/Game/GD_GameResource.cpp
+++ src/Game/GD_GameResource.cpp
@@ -3,10 +3,17 @@
 GD_GameResource* GD_GameResource::myInstance = nullptr;
 
 GD_GameResource::GD_GameResource(){
+    //Game sprites
     if(texture.loadFromFile("../../sprites/digDrillkuSpritesheet.png")
             && font.openFromFile("../../sprites/GlacialIndifference-Regular.otf") ){
         atlas = createDictionary("../../sprites/digDrillkuSpritesheet.atlas");
 	}
+
+    //Screen UI assets
+    if(UITexture.loadFromFile("../../sprites/digDrillkuScreenUI.png")
+        && font.openFromFile("../../sprites/GlacialIndifference-Regular.otf") ){
+        UIAtlas = createDictionary("../../sprites/digDrillkuScreenUI.atlas");
+    }
 }
 
 GD_GameResource* GD_GameResource::createInstance(){
@@ -18,8 +25,14 @@ GD_GameResource* GD_GameResource::createInstance(){
 unordered_map<string,IntRect>* GD_GameResource::getAtlas(){
     return atlas;
 }
+unordered_map<string,IntRect>* GD_GameResource::getUIAtlas(){
+    return UIAtlas;
+}
 Texture* GD_GameResource::getTexture(){
     return &texture;
+}
+Texture* GD_GameResource::getUITexture(){
+    return &UITexture;
 }
 Font* GD_GameResource::getFont(){
     return &font;
--- src/Game/GD_GameResource.h
+++ src/Game/GD_GameResource.h
@@ -7,12 +7,16 @@ class GD_GameResource{
 	public:
 		static GD_GameResource* createInstance();
 		unordered_map<string,IntRect>* getAtlas();
+		unordered_map<string,IntRect>* getUIAtlas();
 		Texture* getTexture();
+		Texture* getUITexture();
 		Font* getFont();
 	private:
 		static GD_GameResource* myInstance;
 		unordered_map<string,IntRect>* atlas;
+		unordered_map<string,IntRect>* UIAtlas;
 		Texture texture;
+		Texture UITexture;
 		Font font;
 		GD_GameResource();
 };
