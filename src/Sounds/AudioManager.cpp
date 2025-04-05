6dd7a087b4a43637dc69fe2001823d30c728c23b
SADIO-M <megan_florence_sadio@dlsu.edu.ph>
2025-04-05 16:51:10 +0800

    little louder

--- src/Sounds/AudioManager.cpp
+++ src/Sounds/AudioManager.cpp
@@ -14,7 +14,7 @@ void AudioManager::loadAllMusic(){
     allBGM.push_back(new Music("../../sounds/MUSIC_win.mp3"));
 
     for(Music* music : allBGM){
-        music->setVolume(30);
+        music->setVolume(8);
         music->setLooping(true);
     }
 }
